f65db4aa4c7a6aaa9857b40f3805fc455d61e632
Svitlana Grytsai <34807666+SvitlanaG@users.noreply.github.com>
2021-04-10 23:10:13 +0200

    feat: solve task 03-sort-by-height

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 /**
  * Given an array with heights, sort them except if the value is -1.
  *
@@ -9,8 +10,16 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const filteredArr = arr.filter((elem) => elem !== -1);
+  const sortedArr = filteredArr.sort((a, b) => a - b);
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      arr.splice(i, 1, sortedArr[count]);
+      count++;
+    }
+  }
+  return arr;
 }
-
 module.exports = sortByHeight;
